025400892cdf05ce8d5c3a131960079d61252f77
Jinx <52132553+JinxHvH@users.noreply.github.com>
2020-08-24 19:57:04 +0100

    Feat: Delete/Update message logs

--- events/messageUpdate.ts
+++ events/messageUpdate.ts
@@ -1,12 +1,24 @@
 import { Message } from 'discord.js';
 import messageEvent from './message';
+import Util from '../util';
 
-export default function messageUpdate(
-	oldMesage: Message,
+export default async function messageUpdate(
+	oldMessage: Message,
 	newMessage: Message
 ) {
 	// handle embed updates
-	if (oldMesage.content === newMessage.content) return;
+	if (oldMessage.content === newMessage.content) return;
 
 	messageEvent(newMessage);
+
+	try {
+		if(oldMessage.author.bot) return;
+		const guildConfig = await newMessage.guild!.fetchConfig();
+
+		if(guildConfig?.logsChannel){
+			await Util.respondWith(guildConfig.logsChannel, 'MESSAGE_UPDATE_LOG', oldMessage, newMessage);
+		}
+	} catch (error) {
+		console.log(error);
+	}
 }
