b08396a5f6a808043bfcc192d2eae83b538a1747
Jinx <52132553+JinxHvH@users.noreply.github.com>
2020-09-08 00:54:05 +0100

    refactor: Checked if suggest length is = or > 1024

--- src/commands/general/suggest.ts
+++ src/commands/general/suggest.ts
@@ -18,7 +18,7 @@ export default class InviteCommand extends Command {
 	async run(message: Message,  args: CommandArguments, send: SendFunction) {
 		if(!args[0]) throw new CommandError('PROVIDE_SUGGESTION');
 		const content = args.join(' ');
-		if(content.length > 1024) throw new CommandError('MAX_MESSAGE_LENGTH');
+		if(content.length >= 1024) throw new CommandError('MAX_MESSAGE_LENGTH');
 		await send('SUGGESTION_RESPONSE');
 		await Util.respondWith(this.client.config.suggestionsChannel!, 'SUGGESTION_LOG', message.author, content);
 	}
