b2e88659a50440dfe6fa9a5c309522f9b63cac38
Jinx <52132553+JinxHvH@users.noreply.github.com>
2020-10-06 01:38:09 +0100

    style: fix indentation

--- src/util/AutoMod.ts
+++ src/util/AutoMod.ts
@@ -5,7 +5,7 @@ export default class AutoMod {
 	static async inviteCheck(msg: Message) {
 		if (
 			!msg.guild!.me!.hasPermission(Permissions.FLAGS.MANAGE_MESSAGES) ||
-      !msg.invites || !msg.invites.length
+			!msg.invites || !msg.invites.length
 		) return false;
 		await msg.delete({ timeout: 100 });
 		await Util.respondWith(msg.channel, 'INVITES_NOT_ALLOWED', msg.author);
